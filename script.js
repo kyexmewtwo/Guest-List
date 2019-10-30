84f1a26768ede404d8e76338de92f03cd75b6e61
kyexmewtwo <45476037+kyexmewtwo@users.noreply.github.com>
2019-10-30 16:39:10 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var friends=[];
-
-$('.addButton').click(function() {
-    var input = $(".guestName").val();
-    console.log(input);
-    friends.push(input);
-    console.log(friends);
-    $(".guestList").append("<li>" + input + "</li>"); 
+var stuff=[];
+var money = [];
+var count=[];
+var comp = [];
+$('.showitems').click(function() {
+    var input = $(".items").val();
+    var price = $(".wow").val();    
+    var numb = $(".number").val();    
+    var tele = price*numb;
+    stuff.push(input);
+    money.push(price);
+    count.push(numb);
+    $(".finish").append("<li>" + count + " " + stuff + "s</li>");
+    $(".finish").append("<li>$" + money*count + "</li>");
+    console.log(stuff);
+    console.log(money);
+    console.log(count);
+    console.log(tele);
+});
+$('.money').click(function(){
+    alert("You will buy ");
 });
